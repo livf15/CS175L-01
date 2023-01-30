fd3b64f6010f735f138ab61e26c7793af15a0da8
olivia <114097318+livf15@users.noreply.github.com>
2023-01-30 13:56:08 -0500

    Restaurant

---
+++ Restaurant Program.py
@@ -0,0 +1,42 @@
+#CS175L
+#Olivia Franczykowski
+#Restaurant
+
+vegetarian=False
+vegan=False
+gluten_free=False
+
+answer1=input("Is anyone in your party a vegetarian?: ")
+if answer1=='yes':
+    vegetarian=True
+
+answer2=input("Is anyone in your party a vegan?: ")
+if answer2=='yes':
+    vegan=True
+
+answer3=input("Is anyone in your party gluten-free?: ")
+if answer3=='yes':
+    gluten_free=True
+print(gluten_free)
+    
+print("Here are your restaurant choices: ")
+
+if vegetarian == False and vegan == False and gluten_free == False:
+    print("   Joe's Gourment Burgers")
+
+if vegetarian == True and vegan == False and gluten_free == True:
+    print("   Main Street Pizza Company")
+
+if vegetarian == True and vegan == False and gluten_free == True:
+    print("   Corner cafe")
+
+if vegetarian == True and vegan == False and gluten_free ==False:
+    print("   Mama's Fine Italian")
+if vegetarian == True and vegan == False and gluten_free == True:
+    print("   The Chef's Kitchen")
+
+            
+
+    
+
+
