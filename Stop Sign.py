931891b91a776fa2f1af7cce93d1ff33c2c4eee8
olivia <114097318+livf15@users.noreply.github.com>
2023-02-13 14:55:58 -0500

    Stop Signs

---
+++ Stop Sign.py
@@ -0,0 +1,28 @@
+import turtle
+s=8
+l=90
+a= 360/s
+diameter= s+2*x
+
+
+turtle.setup(400,400)
+turtle.penup()
+turtle.goto(-50,-60)
+turtle.pendown()
+turtle.pensize(10)
+turtle.color('black','red')
+turtle.begin_fill()
+for x in range(s):
+    turtle.pendown()
+    turtle.forward(l)
+    turtle.left(a)
+turtle.end_fill()
+
+
+turtle.penup()
+turtle.goto(-72,20)
+turtle.color('white')
+turtle.write('STOP',font=('Times New Roman', 60))
+
+
+
