f2c685df52aaa45c97efae7ef0afc6a4d6ab5eed
olivia <114097318+livf15@users.noreply.github.com>
2023-03-09 22:38:44 -0500

    Average From Input with exception

--- AveragefromInput.py
+++ AveragefromInput.py
@@ -5,15 +5,18 @@ def main():
         num1=int(file.readline())
         num2=int(file.readline())
         num3=int(file.readline())
-        print(line)
+        
     
 for line in file:
+    print(line)
     print('I read in 1 number(s)','Current number is:',f'{num1:>.3f}','Total is:', f'{num1:>.3f}')
-    print('I read in 2 number(s)','Current number is:',f'{num2>.3f}','Total is:',f'{num2:>.3f}')
-    print('I read in 3 number(s)','Current number is:',f'{num3>.3f}','Total is:',f'{num3:>.3f}')
+    print('I read in 2 number(s)','Current number is:',f'{num2:>.3f}','Total is:',f'{num2:>.3f}')
+    print('I read in 3 number(s)','Current number is:',f'{num3:>.3f}','Total is:',f'{num3:>.3f}')
     avg=(num1+num2+num3)/3
-    print('The average of the numbers is:', avg)
-        
+    print('The average of the numbers is:', {avg:.3f})
+except fileerror:
+    print('Error:Input file not found')
+
         
     
     
